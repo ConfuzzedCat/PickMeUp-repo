c30cee03916230e14bddce3c1d2da61163e53fed
Pelle Hald Vedsmand <cph-pv73@cphbusiness.dk>
2024-05-07 13:59:10 +0200

    Added method that returns routes based on location and start postal code

--- backend/src/main/java/dk/lyngby/dao/impl/RouteDao.java
+++ backend/src/main/java/dk/lyngby/dao/impl/RouteDao.java
@@ -1,5 +1,10 @@
 package dk.lyngby.dao.impl;
 
+import dk.lyngby.Model.Route;
+import dk.lyngby.config.HibernateConfig;
+import dk.lyngby.dao.IDao;
+import dk.lyngby.exception.ApiException;
+import jakarta.persistence.Query;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
@@ -14,18 +19,74 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.List;
 
-public class RouteDao {
+public class RouteDao implements IDao {
 
+    protected EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
     private static RouteDao instance;
-    private static EntityManagerFactory emf;
 
     public static RouteDao getInstance(EntityManagerFactory _emf) {
         if (instance == null) {
-            emf = _emf;
+            instance = new RouteDao(_emf);
+        }
+        return instance;
+    }
+    
+    public static RouteDao getInstance(){
+        if(instance == null){
             instance = new RouteDao();
         }
         return instance;
     }
+
+    private RouteDao(){}
+    private RouteDao(EntityManagerFactory _emf)
+    {
+        this.emf = _emf;
+    }
+
+
+    public List<Route> getPassengerRoutesWithFilter (String endLocation, String passengerStartPostalCode, Route route){
+        // For the real case we want to define the postal codes that the passenger wants routes for
+        // For this prototype we are gonna limit the routes for the DB call to be the passengers postal code +-100
+        try(EntityManager em = emf.createEntityManager()){
+            Query query = em.createQuery("SELECT r FROM " + Route.class.getSimpleName() + " r WHERE r.endLocation = :endLocation AND r.startPostalCode = :passengerStartPostalCode", Route.class);
+            query.setParameter("endLocation", endLocation);
+            query.setParameter("passengerStartPostalCode", passengerStartPostalCode);
+            return query.getResultList();
+        }
+    }
+
+    @Override
+    public Object read(Object o) throws ApiException {
+        return null;
+    }
+
+    @Override
+    public List readAll() {
+        return null;
+    }
+
+    @Override
+    public Object create(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object update(Object o, Object o2) {
+        return null;
+    }
+
+    @Override
+    public void delete(Object o) {
+
+    }
+
+    @Override
+    public boolean validatePrimaryKey(Object o) {
+        return false;
+    }
+
+
     /** create a search filter for the routes that searches for routes based
      on multiple criteria such as start location, end location, driverId,
      route length, time in minutes, handicap availability, passenger amount,
