710c1d1219b1a11a98124c6fce61a070da7599e9
MrJustMeDahl <nico.dahl93@gmail.com>
2024-05-07 17:47:00 +0200

    Added javadoc

--- backend/src/main/java/dk/lyngby/routes/RoutesRoute.java
+++ backend/src/main/java/dk/lyngby/routes/RoutesRoute.java
@@ -6,6 +6,10 @@ import io.javalin.apibuilder.EndpointGroup;
 import static io.javalin.apibuilder.ApiBuilder.get;
 import static io.javalin.apibuilder.ApiBuilder.path;
 
+/**
+ * @author MrJustMeDahl
+ * This endpointgroup contains endpoints related to routes.
+ */
 public class RoutesRoute {
 
     private final RouteController rc = new RouteController();
