bcac31634ad42dcb70e06c59681329ad4f2f7284
Tobias Tonndorff <cph-tt160@cphbusiness.dk>
2024-05-07 11:18:59 +0200

    routes page

---
+++ frontend/src/Routes.jsx
@@ -0,0 +1,31 @@
+function Routes() {
+    
+    
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return ( 
+        <div>
+            <h1>Routes</h1>
+        </div>
+
+
+     );
+}
+
+export default Routes;
