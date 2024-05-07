3e0b844faa562a4543d07f5a4d4a080bbccb7125
DimitraSis <dimitra.sis7@gmail.com>
2024-05-07 12:10:30 +0200

    fake routesData.js
    #55 create mock route data in frontend

---
+++ frontend/src/routesData.js
@@ -0,0 +1,34 @@
+const routesData = [
+    {
+      id: 1,
+      destination: "Lyngy",
+        handicapAccessibility: true,
+        time: "08:00",
+        date: "2024-05-10",
+        passengers: 4,
+        carSize: "Large",
+        driver: "John Doeson"
+    },
+    {
+      id: 2,
+        destination: "City B",
+            handicapAccessibility: false,
+            time: "09:00",
+            date: "2024-05-11",
+            passengers: 2,
+            carSize: "Medium",
+            driver: "Jane Smithsen"
+    },
+   {
+        id: 3,
+            destination: "City C",
+                handicapAccessibility: false,
+                time: "10:00",
+                date: "2024-05-12",
+                passengers: 3,
+                carSize: "Small",
+                driver: "John Doesen"
+   }
+  ];
+  
+  export default routesData;
