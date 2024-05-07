f3fd42a072b7a8a8976faff462ae0b8474af1e5a
MrJustMeDahl <nico.dahl93@gmail.com>
2024-05-07 16:50:59 +0200

    Added utility to the route calculator

--- backend/src/main/java/dk/lyngby/controller/impl/RouteController.java
+++ backend/src/main/java/dk/lyngby/controller/impl/RouteController.java
@@ -1,17 +1,42 @@
 package dk.lyngby.controller.impl;
 
-import dk.lyngby.controller.IController;
 import dk.lyngby.exception.ApiException;
+import dk.lyngby.utility.RouteCalcUtil;
 import io.javalin.http.Context;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RouteController {
 
-    public void getListOfRoutesClosestToStart(Context ctx){
+    private RouteCalcUtil routeUtil = new RouteCalcUtil();
+
+    public void getListOfRoutesClosestToStart(Context ctx) throws ApiException{
         // Hiv start og slut ud af context.
+        RequestBody requestBody = ctx.bodyAsClass(RequestBody.class);
+        String startLocationCoords = routeUtil.getCoordinatesForAddress(requestBody.getStartLocation());
         // Hent driver routes i DB, som har samme slutpunkt.
         // Sammenlign distancen mellem startpunktet for brugeren og startpunktet for routen, via geoapify.
+        Map<Route, Double> chosenRoutes = new HashMap<Route, Double>();
+        for(Route r: routes){
+            String routeCoords = routeUtil.getCoordinatesForAddress(r.getStartLocation());
+            double distance = routeUtil.findDistanceBetweenTwoLocations(startLocationCoords, routeCoords);
+            if(distance >= 0) {
+                chosenRoutes.put(r, distance);
+            }
+        }
         // Filtrér routes fra som er urealistike / ikke passer til filtrering.
         // Sortér routes sådan at de routes som er tættest på brugerens start lokation bliver vist først.
 
+    }
+
+    private static class RequestBody {
+
+        private String startLocation;
+        private String endLocation;
+
+        private String getStartLocation(){
+            return startLocation;
+        }
     }
 }
--- backend/src/main/java/dk/lyngby/utility/RouteCalcUtil.java
+++ backend/src/main/java/dk/lyngby/utility/RouteCalcUtil.java
@@ -1,2 +1,51 @@
-package dk.lyngby.utility;public class RouteCalcUtil {
+package dk.lyngby.utility;
+
+import com.google.gson.Gson;
+import dk.lyngby.dto.CoordinateDTO;
+import dk.lyngby.dto.DistanceDTO;
+import dk.lyngby.exception.ApiException;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class RouteCalcUtil {
+
+    OkHttpClient client = new OkHttpClient().newBuilder().build();
+    Gson gson = new Gson();
+
+    public String getCoordinatesForAddress(String location) throws ApiException{
+        String requestString = "https://api.geoapify.com/v1/geocode/search?text=" + location + "&format=json&apiKey=c205e64d77a04951959d6de10d7e5c2e";
+        Request request = new Request.Builder()
+                .url(requestString)
+                .method("GET", null)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            CoordinateDTO dto = gson.fromJson(response.body().string(), CoordinateDTO.class);
+
+            return dto.getResults().getLon() + "," + dto.getResults().getLat();
+        } catch(IOException e){
+            throw new ApiException(500, "Failed to convert address: " + location + " to coordinates. Something went wrong.");
+        }
+    }
+
+    public double findDistanceBetweenTwoLocations(String startLocationCoords, String routeCoords) throws ApiException {
+        String requestString = "https://api.geoapify.com/v1/routing?waypoints=" + startLocationCoords + "|" + routeCoords + "&mode=drive&apiKey=c205e64d77a04951959d6de10d7e5c2e";
+        Request request = new Request.Builder()
+                .url(requestString)
+                .method("GET", null)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            DistanceDTO distanceDTO = gson.fromJson(response.body().string(), DistanceDTO.class);
+
+            return distanceDTO.getFeatures().getProperties().getDistance();
+        } catch (IOException e){
+            throw new ApiException(500, "Failed to retrieve distance between the 2 locations. Something went wrong");
+        }
+    }
 }
