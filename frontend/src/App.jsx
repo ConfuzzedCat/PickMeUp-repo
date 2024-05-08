692cfa9f71b5af076c8a96080aa91d091d7738dd
Andreas Pihl <54024731+bakkary@users.noreply.github.com>
2024-05-08 10:57:22 +0200

    tailwind login form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,40 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Route, NavLink, Routes } from 'react-router-dom';
-import './App.css'
-import Signup from './signup';
-import Upload from './upload';
+import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Routes,
+} from "react-router-dom";
+import "./App.css";
+import Signup from "./signup";
+import Upload from "./upload";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    
     <Router>
-    <header>
-     <ul>
-      <li><NavLink to ="/signup" activeClassName="active">Signup</NavLink></li>
-      <li><NavLink to ="/upload" activeClassName="active">Upload</NavLink></li>
-     </ul>
+      <header>
+        <ul>
+          <li>
+            <NavLink to="/signup" activeClassName="active">
+              Signup
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/upload" activeClassName="active">
+              Upload
+            </NavLink>
+          </li>
+        </ul>
       </header>
-      
-    <Routes>
+
+      <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/upload" element={<Upload />} />
-    </Routes>
-
+      </Routes>
     </Router>
-
-  )
-  
+  );
 }
 
 export default App;
--- frontend/src/upload.jsx
+++ frontend/src/upload.jsx
@@ -1,27 +1,27 @@
 import React, { useState } from "react";
 
 function Upload() {
-    const [file, setFile] = useState(null);
+  const [file, setFile] = useState(null);
 
-    const handleFileChange = (event) => {
-        setFile(event.target.files[0]);
-    };
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
 
-    const uploadFile = async () => {
-        if (!file) {
-            alert("Please select a file first.");
-            return;
-        }
-        console.log("Uploading", file);
-    };
+  const uploadFile = async () => {
+    if (!file) {
+      alert("Please select a file first.");
+      return;
+    }
+    console.log("Uploading", file);
+  };
 
-    return (
-        <div>
-            <h1>Upload File</h1>
-            <input type="file" onChange={handleFileChange} />
-            <button onClick={uploadFile}>Upload</button>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Upload File</h1>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={uploadFile}>Upload</button>
+    </div>
+  );
 }
 
 export default Upload;
