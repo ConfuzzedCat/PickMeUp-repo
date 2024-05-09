b9edc8d1755efeb24cb2294766e364304fc9c30f
ConfuzzedCat <confuzzedcat@gmail.com>
2024-05-09 16:18:37 +0200

    Made Route entity ready for db

--- backend/src/main/java/dk/lyngby/model/Route.java
+++ backend/src/main/java/dk/lyngby/model/Route.java
@@ -1,4 +1,8 @@
 package dk.lyngby.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 import java.time.LocalTime;
 
@@ -6,8 +10,11 @@ import java.time.LocalTime;
 @Setter
 @NoArgsConstructor
 @ToString
-
+@Entity
 public class Route {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     private String startLocation;
     private String endLocation;
     private int driverId;
