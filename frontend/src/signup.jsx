5b7ce17c7696c54f794f823fa3e35e23855b97c4
Andreas Pihl <54024731+bakkary@users.noreply.github.com>
2024-05-15 10:26:25 +0200

    better scaling of signup

--- frontend/src/signup.jsx
+++ frontend/src/signup.jsx
@@ -82,128 +82,132 @@ function Signup() {
 
 
   return (
-    <section clas="bg-gray-50 dark:bg-gray-900">
-      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign up to a drivers account
-            </h1>
-            <form onChange={onChange}
-            
-              class="space-y-4 md:space-y-6"
-              action="#"
-              onSubmit={performSignup}
+<>
+<section class="bg-gray-800 dark:bg-gray-900">
+  <div class="flex flex-col items-center justify-center px-6 py-4 mx-auto md:h-screen lg:py-0">
+    <div class="w-full bg-gray-700 dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 rounded-lg shadow">
+      <div class="p-4 space-y-2 md:space-y-4 sm:p-6">
+        <h1 class="text-lg font-bold leading-tight tracking-tight text-white md:text-xl dark:text-white">
+          Sign up to a driver's account
+        </h1>
+        <form
+          onChange={onChange}
+          class="space-y-2 md:space-y-4"
+          action="#"
+          onSubmit={performSignup}
+        >
+          <div>
+            <label
+              htmlFor="email"
+              class="block mb-1 text-sm font-medium text-white dark:text-white"
             >
-              <div>
-                <label
-                  htmlFor="email"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                    ref={email}
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@company.com"
-
-                  required=""
-                ></input>
-              </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  ref={password}
-                  placeholder="••••••••"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                ></input>
-              </div>
-              <div>
-                <label
-                  htmlFor="address"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Address
-                </label>
-                <input
-                  type="text"
-                  name="address"
-                  id="address"
-                    ref={address}
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                ></input>
-              </div>
-              <div>
-                <label
-                  htmlFor="fullname"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Full Name
-                </label>
-                <input
-                  type="text"
-                  name="fullname"
-                  id="fullname"
-                  ref={fullname}
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                ></input>
-              </div>
-              <div>
-                <label htmlFor="licenseImage" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Upload your driver's license
-                </label>
-                <input
-                    type="file"
-                    id="licenseImage"
-                    ref={licenseImage}
-                    name="licenseImage"
-                    accept="image/*" 
-                    onChange={onFileChange}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-                    required=""
-                />
-                </div>
-                <div>
-                <label htmlFor="licenseImage" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Upload your Student ID
-                </label>
-                <input
-                    type="file"
-                    id="StudentImage"
-                    ref={StudentImage}
-                    name="StudentImage"
-                    accept="image/*" 
-                    onChange={onFileChange}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-                    required=""
-                />
-                </div>
-              
-              <button 
-                type="submit"
-                class="w-full text-gray-800 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                Sign up
-              </button>
-            </form>
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              ref={email}
+              class="bg-gray-600 border border-gray-500 text-white sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="name@company.com"
+              required=""
+            />
           </div>
-        </div>
+          <div>
+            <label
+              htmlFor="password"
+              class="block mb-2 text-sm font-medium text-white dark:text-white"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              ref={password}
+              placeholder="••••••••"
+              class="bg-gray-600 border border-gray-500 text-white sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required=""
+            ></input>
+          </div>
+          <div>
+            <label
+              htmlFor="address"
+              class="block mb-2 text-sm font-medium text-white dark:text-white"
+            >
+              Address
+            </label>
+            <input
+              type="text"
+              name="address"
+              id="address"
+              ref={address}
+              class="bg-gray-600 border border-gray-500 text-white sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required=""
+            ></input>
+          </div>
+          <div>
+            <label
+              htmlFor="fullname"
+              class="block mb-2 text-sm font-medium text-white dark:text-white"
+            >
+              Full Name
+            </label>
+            <input
+              type="text"
+              name="fullname"
+              id="fullname"
+              ref={fullname}
+              class="bg-gray-600 border border-gray-500 text-white sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required=""
+            ></input>
+          </div>
+          <div>
+            <label htmlFor="licenseImage" className="block mb-2 text-sm font-medium text-white dark:text-white">
+                Upload your driver's license
+            </label>
+            <input
+                type="file"
+                id="licenseImage"
+                ref={licenseImage}
+                name="licenseImage"
+                accept="image/*" 
+                onChange={onFileChange}
+                className="bg-gray-600 border border-gray-500 text-white text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                required=""
+            />
+          </div>
+          <div>
+            <label htmlFor="licenseImage" className="block mb-2 text-sm font-medium text-white dark:text-white">
+                Upload your Student ID
+            </label>
+            <input
+                type="file"
+                id="StudentImage"
+                ref={StudentImage}
+                name="StudentImage"
+                accept="image/*" 
+                onChange={onFileChange}
+                className="bg-gray-600 border border-gray-500 text-white text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                required=""
+            />
+          </div>
+        
+          <button 
+            type="submit"
+            class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          >
+            Sign up
+          </button>
+        </form>
       </div>
-    </section>
+    </div>
+  </div>
+</section>
+
+</>
+
+
   );
 }
 
