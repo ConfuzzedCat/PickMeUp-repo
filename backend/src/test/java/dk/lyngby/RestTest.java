ced3958fddce315059437a1da4ef9f0bb333a4e4
MrJustMeDahl <nico.dahl93@gmail.com>
2024-05-17 22:21:19 +0200

    Updated teardown to run afterAll

--- backend/src/test/java/dk/lyngby/RestTest.java
+++ backend/src/test/java/dk/lyngby/RestTest.java
@@ -74,7 +74,7 @@ public class RestTest {
             em.getTransaction().commit();
         }
     }
-    @AfterEach
+    @AfterAll
     void tearDown() {
         HibernateConfig.setTest(false);
         ApplicationConfig.stopServer(app);
